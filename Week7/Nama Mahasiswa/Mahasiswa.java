8b29d1e6e3c62621e83c5e1e23f07eb70374ecb9
Jamaludin <61264944+jamalludin13@users.noreply.github.com>
2020-04-22 14:44:12 +0700

    Add files via upload

---
+++ Week7/Nama Mahasiswa/Mahasiswa.java
@@ -0,0 +1,21 @@
+public class Mahasiswa {
+
+    public static void main(String[] args) { 
+	
+        System.out.println(---------"Materi selama Bootcamp-------");
+		
+        System.out.println("Minggu ke 1 :");
+		System.out.println("1. Pengenalan Java" \n "Java OOP ");
+		
+		System.out.println("Minggu ke 2 :");
+		System.out.println("1. J2SE" \n "J2EE ");
+		
+		System.out.println("Minggu ke 3 :");
+		System.out.println("1. Pengenalan Framework" \n "Framework Spring ");
+		
+		 
+		
+		
+
+    }
+}
---
+++ Week7/Nama Mahasiswa/Mahasiswa2.java
@@ -0,0 +1,8 @@
+public class Mahasiswa {
+
+    public static void main(String[] args) { 
+        System.out.println(---------"Daftar Mahasiswa-------");
+        System.out.println("1. Jamaludin \n 2. Agas \n 3. Yoga \n Sandi \n Riska ");
+
+    }
+}
